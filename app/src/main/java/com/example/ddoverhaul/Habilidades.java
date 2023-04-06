bebf70c02986169cd7866e4c901cde36246c2ba3
Guillermo <grm0004@alu.medac.es>
2023-04-06 20:02:30 +0200

    Atributos de Habilidades

--- app/src/main/java/com/example/ddoverhaul/Habilidades.java
+++ app/src/main/java/com/example/ddoverhaul/Habilidades.java
@@ -1,4 +1,29 @@
 package com.example.ddoverhaul;
 
 public class Habilidades {
+    String nombre;
+    int vida;
+    int vida_Mx;
+    int mana;
+    int mana_Mx;
+    String raza;
+    String Clase;
+    /*Estadisticas*/
+    int fuerza;
+    int destreza;
+    int constitucion;
+    int inteligencia;
+    int sabiduria;
+    int carsima;
+    int velocidad;
+    boolean vivo;
+    /*Equipo*/
+    String Arma;
+    String Arma_Sec;
+    String Perchera;
+    String Pantalones;
+    String Guantes;
+    String Pies;
+    String Accesorios[];
+
 }
