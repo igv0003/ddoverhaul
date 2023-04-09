844cc84623951231f223befd050cc845076d87b6
Guillermo <grm0004@alu.medac.es>
2023-04-09 21:01:34 +0200

    Contructor a medias de Personajes

--- app/src/main/java/com/example/ddoverhaul/Personaje.java
+++ app/src/main/java/com/example/ddoverhaul/Personaje.java
@@ -1,5 +1,7 @@
 package com.example.ddoverhaul;
 
+import java.util.ArrayList;
+
 public class Personaje {
     String nombre;
     private int vida;
@@ -26,116 +28,102 @@ public class Personaje {
     Objeto Pies;
     Objeto Accesorios;
 
+    ArrayList<Objeto> inventario; /*Inventario es un arrayList de Objetos*/
+
     /*Get Y Set*/
     public void setVida(int vida) {
         this.vida = vida;
     }
-
     public int getVida() {
         return vida;
     }
-
     public int getVida_Mx() {
         return vida_Mx;
     }
-
     public void setVida_Mx(int vida_Mx) {
         this.vida_Mx = vida_Mx;
     }
-
     public int getMana() {
         return mana;
     }
-
     public void setMana(int mana) {
         this.mana = mana;
     }
-
     public int getMana_Mx() {
         return mana_Mx;
     }
-
     public void setMana_Mx(int mana_Mx) {
         this.mana_Mx = mana_Mx;
     }
-
     public String getRaza() {
         return raza;
     }
-
     public void setRaza(String raza) {
         this.raza = raza;
     }
-
     public String getClase() {
         return Clase;
     }
-
     public void setClase(String clase) {
         Clase = clase;
     }
-
     public int getFuerza() {
         return fuerza;
     }
-
     public void setFuerza(int fuerza) {
         this.fuerza = fuerza;
     }
-
     public int getDestreza() {
         return destreza;
     }
-
     public void setDestreza(int destreza) {
         this.destreza = destreza;
     }
-
     public int getConstitucion() {
         return constitucion;
     }
-
     public void setConstitucion(int constitucion) {
         this.constitucion = constitucion;
     }
-
     public int getInteligencia() {
         return inteligencia;
     }
-
     public void setInteligencia(int inteligencia) {
         this.inteligencia = inteligencia;
     }
-
     public int getSabiduria() {
         return sabiduria;
     }
-
     public void setSabiduria(int sabiduria) {
         this.sabiduria = sabiduria;
     }
-
     public int getCarsima() {
         return carsima;
     }
-
     public void setCarsima(int carsima) {
         this.carsima = carsima;
     }
-
     public int getVelocidad() {
         return velocidad;
     }
-
     public void setVelocidad(int velocidad) {
         this.velocidad = velocidad;
     }
-
     public boolean isVivo() {
         return vivo;
     }
-
     public void setVivo(boolean vivo) {
         this.vivo = vivo;
+    }
+
+    /*CONSTRUCTORES*/
+
+    public Personaje(String name, int life, int energy, String race){
+        this.nombre = name;
+        this.vida_Mx = life;
+        this.vida = life;
+        this.mana_Mx = energy;
+        this.mana = energy;
+        this.raza = race;
     }
 }
