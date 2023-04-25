5df3c239b3edae63d5899db1f5d6358eef899fc3
GRM <grm0004@alu.medac.es>
2023-04-25 19:25:36 +0200

    Metodos Habilidades

--- app/src/main/java/com/example/ddoverhaul/Metodos.java
+++ app/src/main/java/com/example/ddoverhaul/Metodos.java
@@ -3,35 +3,61 @@ package com.example.ddoverhaul;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Metodos extends AppCompatActivity {
-
+    /*OBJETOS*/
     public void anadirObj(Objeto obj){
         /*METER OBJETO MEDIANTE JSON*/
     };
-
+    /*Metodo con icono*/
     public void addObject(String nombre, String tipo, String descripcion, String icono){
         int getLastId=0; /*Metodo que obtiene el ultimo id*/
         int id = getLastId;
         Objeto obj = new Objeto(id, nombre, tipo, descripcion, icono);
         anadirObj(obj);
     }
+    /*Metodo sin icono*/
     public void addObject(String nombre, String tipo, String descripcion){
         int getLastId=0;
         int id = getLastId;/*Metodo que obtiene el ultimo id*/
         Objeto obj = new Objeto(id,nombre, tipo, descripcion);
         anadirObj(obj);
     }
+
+    /*EVENTOS*/
+    public void anadirEvent(Evento evn){
+        /*METER OBJETO MEDIANTE JSON*/
+    };
     /*Metodo con objeto*/
     public void addEvento(String descripcion, int valor, int cantidad, char operacion, Objeto obj){
         int getLastId=0;
         int id = getLastId;/*Metodo que obtiene el ultimo id*/
         Evento event = new Evento(id,  descripcion, valor, cantidad, operacion, obj);
-
+        anadirEvent(event);
     }
     /*Metodo sin Objeto*/
     public void addEvento(String descripcion, int valor, int cantidad, char operacion){
         int getLastId=0;
         int id = getLastId;/*Metodo que obtiene el ultimo id*/
         Evento event = new Evento(id,  descripcion, valor, cantidad, operacion);
+        anadirEvent(event);
+    }
+
+    /*HEBILIDADES*/
+    public void anadirHabili(Habilidades habili){
+        /*METER OBJETO MEDIANTE JSON*/
+    };
+    /*Metodo con Icono*/
+    public void addHabili(String nombre, int coste, int danio, String problema_estado, int porcentaje, String descripcion, String icono){
+        int getLastId=0;
+        int id = getLastId;/*Metodo que obtiene el ultimo id*/
+        Habilidades habili = new Habilidades(id, nombre, coste,danio, problema_estado, porcentaje,descripcion, icono);
+        anadirHabili(habili);
+    }
+    /*Metodo sin Icono*/
+    public void addHabili(String nombre, int coste, int danio, String problema_estado, int porcentaje, String descripcion){
+        int getLastId=0;
+        int id = getLastId;/*Metodo que obtiene el ultimo id*/
+        Habilidades habili = new Habilidades(id, nombre, coste,danio, problema_estado, porcentaje,descripcion);
+        anadirHabili(habili);
     }
 
 
