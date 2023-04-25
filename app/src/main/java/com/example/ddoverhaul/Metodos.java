879a2807c4ad9943daca043f46c6ba3140c31e03
GRM <grm0004@alu.medac.es>
2023-04-25 19:15:02 +0200

    Metodos Evento

--- app/src/main/java/com/example/ddoverhaul/Metodos.java
+++ app/src/main/java/com/example/ddoverhaul/Metodos.java
@@ -1,10 +1,12 @@
 package com.example.ddoverhaul;
-
+/*METODOS PARA POSTERIORES MOVIMIENTOS*/
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Metodos extends AppCompatActivity {
 
-    public void anadirObj(Objeto obj){};
+    public void anadirObj(Objeto obj){
+        /*METER OBJETO MEDIANTE JSON*/
+    };
 
     public void addObject(String nombre, String tipo, String descripcion, String icono){
         int getLastId=0; /*Metodo que obtiene el ultimo id*/
@@ -12,23 +14,25 @@ public class Metodos extends AppCompatActivity {
         Objeto obj = new Objeto(id, nombre, tipo, descripcion, icono);
         anadirObj(obj);
     }
-
-
     public void addObject(String nombre, String tipo, String descripcion){
         int getLastId=0;
         int id = getLastId;/*Metodo que obtiene el ultimo id*/
         Objeto obj = new Objeto(id,nombre, tipo, descripcion);
         anadirObj(obj);
     }
+    /*Metodo con objeto*/
+    public void addEvento(String descripcion, int valor, int cantidad, char operacion, Objeto obj){
+        int getLastId=0;
+        int id = getLastId;/*Metodo que obtiene el ultimo id*/
+        Evento event = new Evento(id,  descripcion, valor, cantidad, operacion, obj);
 
+    }
+    /*Metodo sin Objeto*/
     public void addEvento(String descripcion, int valor, int cantidad, char operacion){
         int getLastId=0;
         int id = getLastId;/*Metodo que obtiene el ultimo id*/
-
+        Evento event = new Evento(id,  descripcion, valor, cantidad, operacion);
     }
 
-    public void addEvento( int valor, int cantidad, char operacion){
-        int getLastId=0;
-        int id = getLastId;/*Metodo que obtiene el ultimo id*/
-    }
+
 }
