465fddc68c29938300f892373935236f3203ce54
Jose Manuel <jbm0008@alu.medac.es>
2023-04-27 22:09:28 +0200

    login y registrar listos para ser utilizados

--- app/src/main/java/com/example/ddoverhaul/Login.java
+++ app/src/main/java/com/example/ddoverhaul/Login.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -13,16 +14,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class Login extends AppCompatActivity {
     private FirebaseAuth mAuth;
+    EditText Email;
+    EditText NameE;
+    EditText PasswordE;
+    String EmailS;
+    String NameS;
+    String PasswordS;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         mAuth = FirebaseAuth.getInstance();
+
+        Email = findViewById(R.id.email);
+        PasswordE = findViewById(R.id.Password);
         Button blog = findViewById(R.id.blog);
+
+
         blog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                signIn("jbm0008@alu.medac.es","MEDACC");
+                EmailS = Email.getText().toString();
+                PasswordS = PasswordE.getText().toString();
+                signIn(EmailS,PasswordS);
             }
         });
     }
